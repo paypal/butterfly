45ca381353889bd9376949b5f123a97b32438a37
facarvalho <facarvalho@paypal.com>
2016-07-01 16:56:45 -0500

    Making Core component Spring Boot auto-configurable

---
+++ butterfly-core/src/main/java/com/paypal/butterfly/core/ButterflyCoreConfig.java
@@ -0,0 +1,13 @@
+package com.paypal.butterfly.core;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Spring Boot configuration class to automatically
+ * register Butterfly Core Spring beans
+ *
+ * @author facarvalho
+ */
+@ComponentScan("com.paypal.butterfly.core")
+public class ButterflyCoreConfig {
+}
