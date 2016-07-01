eb7349f364a95bb91ca05cfcc3a0203bca2bf0d6
facarvalho <facarvalho@paypal.com>
2016-07-01 11:14:34 -0500

    Making CLI project SpringBoot based Plus, adding Spring Boot BOM to parent project

---
+++ butterfly-cli/src/main/java/com/paypal/butterfly/cli/ButterflyCliApplication.java
@@ -0,0 +1,13 @@
+package com.paypal.butterfly.cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButterflyCliApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(ButterflyCliApplication.class, args);
+    }
+
+}
