3f2c6873ef01e25402687f12685fa54dd7b1aa18
facarvalho <facarvalho@paypal.com>
2016-07-12 15:56:12 -0500

    Adding FindFile and FindFiles basic utilities

---
+++ butterfly-basic-operations/src/main/java/com/paypal/butterfly/basic/utilities/file/FindFile.java
@@ -0,0 +1,78 @@
+package com.paypal.butterfly.basic.utilities.file;
+
+import com.paypal.butterfly.extensions.api.TransformationContext;
+import com.paypal.butterfly.extensions.api.TransformationUtility;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.AbstractFileFilter;
+import org.apache.commons.io.filefilter.IOFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Utility to find a file based on its name. The search is
+ * always recursive (includes sub-folders). If multiple files
+ * are found, the first one only is returned. If no file is
+ * found, null is returned.
+ *
+ * @see {@link FindFiles} for a better refined search
+ *
+ * @author facarvalho
+ */
+public class FindFile extends TransformationUtility<FindFile, File> {
+
+    private static final Logger logger = LoggerFactory.getLogger(FindFile.class);
+
+    private static final String DESCRIPTION = "Find file named %s under %s";
+
+    private String fileName;
+
+    /**
+     * Utility to find a file based on its name
+     *
+     * @see {@link #setRelativePath(String)}
+     * @param relativePath
+     */
+    private FindFile(String relativePath) {
+        super(relativePath);
+    }
+
+    /**
+     * Utility to find a file based on its name
+     *
+     * @see {@link #setRelativePath(String)}
+     * @param relativePath
+     * @param fileName name of the file to be found
+     */
+    public FindFile(String relativePath, String fileName) {
+        this(relativePath);
+        this.fileName = fileName;
+    }
+
+    @Override
+    public String getDescription() {
+        return String.format(DESCRIPTION, fileName, getRelativePath());
+    }
+
+    @Override
+    protected File execution(File transformedAppFolder, TransformationContext transformationContext) throws Exception {
+        FindFiles findFiles = new FindFiles(getRelativePath(), fileName, true);
+        List<File> files = findFiles.execution(transformedAppFolder, transformationContext);
+
+        if(files == null || files.size() == 0) {
+            logger.debug("No file named {} has been found by {}", fileName, getName());
+            return null;
+        }
+        if(files.size() > 1) {
+            logger.debug("More than one file named {} has been found by {}, the first one will be returned", fileName, getName());
+        }
+
+        return files.get(0);
+    }
+
+}
---
+++ butterfly-basic-operations/src/main/java/com/paypal/butterfly/basic/utilities/file/FindFiles.java
@@ -0,0 +1,71 @@
+package com.paypal.butterfly.basic.utilities.file;
+
+import com.paypal.butterfly.extensions.api.TransformationContext;
+import com.paypal.butterfly.extensions.api.TransformationUtility;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.AbstractFileFilter;
+import org.apache.commons.io.filefilter.IOFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Utility to find files based on a regular expression
+ * against the file name. The search might be recursive (including sub-folders)or not
+ *
+ * @author facarvalho
+ */
+public class FindFiles extends TransformationUtility<FindFiles, List<File>> {
+
+    private static final String DESCRIPTION = "Find files whose name accepts regular expression %s and are under folder %s%s";
+
+    private String regex;
+    private boolean recursive;
+
+    /**
+     * Utility to find files
+     *
+     * @see {@link #setRelativePath(String)}
+     * @param relativePath
+     */
+    private FindFiles(String relativePath) {
+        super(relativePath);
+    }
+
+    /**
+     * Utility to find a files
+     *
+     * @see {@link #setRelativePath(String)}
+     * @param relativePath
+     * @param regex regular expression to be applied against file name during search
+     * @param recursive if true, sub-folders will also be searched
+     */
+    public FindFiles(String relativePath, String regex, boolean recursive) {
+        this(relativePath);
+        this.regex = regex;
+        this.recursive = recursive;
+    }
+
+    @Override
+    public String getDescription() {
+        return String.format(DESCRIPTION, regex, getRelativePath(), (recursive ? " and sub-folders" : " only (not including sub-folders)"));
+    }
+
+    @Override
+    protected List<File> execution(File transformedAppFolder, TransformationContext transformationContext) throws Exception {
+        File rootSearchFolder = getAbsoluteFile(transformedAppFolder, transformationContext);
+
+        IOFileFilter fileFilter = new AbstractFileFilter() {
+            public boolean accept(File file) {
+                return (file.isFile() && file.getName().matches(regex));
+            }
+        };
+        Collection<File> files = FileUtils.listFiles(rootSearchFolder, fileFilter, (recursive ? TrueFileFilter.INSTANCE : null));
+
+        return new ArrayList<File>(files);
+    }
+
+}
