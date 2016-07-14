0f60658cef826b5b1d7b9ed30b8925086f488a3e
facarvalho <facarvalho@paypal.com>
2016-07-14 09:35:07 -0500

    Adding LocateFile basic transformation utility

---
+++ butterfly-basic-operations/src/main/java/com/paypal/butterfly/basic/utilities/file/LocateFile.java
@@ -0,0 +1,44 @@
+package com.paypal.butterfly.basic.utilities.file;
+
+import com.paypal.butterfly.extensions.api.TransformationContext;
+import com.paypal.butterfly.extensions.api.TransformationUtility;
+
+import java.io.File;
+
+/**
+ * Utility to locate a file based on the relative or absolute
+ * locations specified. It does not find files, it just results
+ * to a {@link File} object based on the input information.
+ *
+ * @author facarvalho
+ */
+public class LocateFile extends TransformationUtility<LocateFile, File> {
+
+    private static final String DESCRIPTION = "Locate file under %s";
+
+    public LocateFile() {
+    }
+
+    /**
+     * Utility to locate a file based on the relative or absolute
+     * locations specified. It does not find files, it just results
+     * to a File object based on the input information
+     *
+     * @see {@link #setRelativePath(String)}
+     * @param relativePath
+     */
+    private LocateFile(String relativePath) {
+        super(relativePath);
+    }
+
+    @Override
+    public String getDescription() {
+        return String.format(DESCRIPTION, getRelativePath());
+    }
+
+    @Override
+    protected File execution(File transformedAppFolder, TransformationContext transformationContext) throws Exception {
+        return getAbsoluteFile(transformedAppFolder, transformationContext);
+    }
+
+}
