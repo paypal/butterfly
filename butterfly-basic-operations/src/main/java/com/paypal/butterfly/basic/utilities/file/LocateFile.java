b56330d7fcb878675934d3b6d9ace1ead7e69893
facarvalho <facarvalho@paypal.com>
2016-07-15 13:43:52 -0500

    Adding parent levels support to LocateFile

--- butterfly-basic-operations/src/main/java/com/paypal/butterfly/basic/utilities/file/LocateFile.java
+++ butterfly-basic-operations/src/main/java/com/paypal/butterfly/basic/utilities/file/LocateFile.java
@@ -9,6 +9,8 @@ import java.io.File;
  * Utility to locate a file based on the relative or absolute
  * locations specified. It does not find files, it just results
  * to a {@link File} object based on the input information.
+ * This utility also allows to locate a file going up in parent
+ * levels from the specified file.
  *
  * @author facarvalho
  */
@@ -16,7 +18,31 @@ public class LocateFile extends TransformationUtility<LocateFile, File> {
 
     private static final String DESCRIPTION = "Locate file under %s";
 
+    private int parentLevel = 0;
+
     public LocateFile() {
+    }
+
+    /**
+     * Set how many parent levels up the location process should
+     * go through. If not set, the actual specified file will be
+     * the located one.
+     *
+     * @param parentLevel how many parent levels to be located
+     *
+     * @return this instance
+     */
+    public LocateFile setParentLevel(int parentLevel) {
+        if(parentLevel < 0) {
+            throw new IllegalArgumentException("Invalid parent level");
+        }
+
+        this.parentLevel = parentLevel;
+        return this;
+    }
+
+    public int getParentLevel() {
+        return parentLevel;
     }
 
     @Override
@@ -26,7 +52,13 @@ public class LocateFile extends TransformationUtility<LocateFile, File> {
 
     @Override
     protected File execution(File transformedAppFolder, TransformationContext transformationContext) throws Exception {
-        return getAbsoluteFile(transformedAppFolder, transformationContext);
+        File locatedFile = getAbsoluteFile(transformedAppFolder, transformationContext);
+        for(int i = parentLevel; i > 0; i--) {
+            locatedFile = locatedFile.getParentFile();
+        }
+
+        return locatedFile;
+
     }
 
 }
