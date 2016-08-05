5eb619c2d8fcf416df452c98ffba15645a4b9f9c
facarvalho <facarvalho@paypal.com>
2016-08-05 16:51:21 -0500

    Adding ApplyZip transformation operation

--- butterfly-basic-operations/src/main/java/com/paypal/butterfly/basic/operations/file/ApplyZip.java
+++ butterfly-basic-operations/src/main/java/com/paypal/butterfly/basic/operations/file/ApplyZip.java
@@ -2,9 +2,20 @@ package com.paypal.butterfly.basic.operations.file;
 
 import com.paypal.butterfly.extensions.api.TransformationContext;
 import com.paypal.butterfly.extensions.api.TransformationOperation;
+import com.paypal.butterfly.extensions.api.exception.ButterflyRuntimeException;
+import com.paypal.butterfly.extensions.api.exception.TransformationDefinitionException;
+import com.paypal.butterfly.extensions.api.exception.TransformationOperationException;
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.model.ZipParameters;
+import net.lingala.zip4j.util.Zip4jConstants;
+import org.apache.commons.io.FileUtils;
 
-import java.io.File;
+import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 
 /**
  * Operation to apply the contents of a zip file
@@ -23,10 +34,23 @@ public class ApplyZip extends TransformationOperation<ApplyZip> {
     /**
      * Operation to apply the contents of a zip file
      *
+     * @param zipFileUrl URL string to locate the zip file to be applied
+     */
+    public ApplyZip(String zipFileUrl) {
+        try {
+            setZipFileUrl(new URL(zipFileUrl));
+        } catch (MalformedURLException e) {
+            throw new TransformationDefinitionException("Malformed Zip file URL", e);
+        }
+    }
+
+    /**
+     * Operation to apply the contents of a zip file
+     *
      * @param zipFileUrl URL to locate the zip file to be applied
      */
     public ApplyZip(URL zipFileUrl) {
-        this.zipFileUrl = zipFileUrl;
+        setZipFileUrl(zipFileUrl);
     }
 
     public ApplyZip setZipFileUrl(URL zipFileUrl) {
@@ -45,9 +69,23 @@ public class ApplyZip extends TransformationOperation<ApplyZip> {
 
     @Override
     protected String execution(File transformedAppFolder, TransformationContext transformationContext) throws Exception {
-        // TODO
+        // Folder where the zip file is supposed to be extracted
+        File folder = getAbsoluteFile(transformedAppFolder, transformationContext);
 
-        return null;
+        ReadableByteChannel readableByteChannel = Channels.newChannel(zipFileUrl.openStream());
+
+        int p = zipFileUrl.getPath().lastIndexOf("/") + 1;
+        String fileName = zipFileUrl.getPath().substring(p);
+
+        File zipFileDescriptor = new File(folder, fileName);
+        FileOutputStream fileOutputStream = new FileOutputStream(zipFileDescriptor);
+        fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+
+        ZipFile zipFile = new ZipFile(zipFileDescriptor);
+        zipFile.extractAll(zipFileDescriptor.getParent());
+        FileUtils.deleteQuietly(zipFileDescriptor);
+
+        return String.format("Zip file %s has been downloaded and decompressed into %s", zipFileUrl, getRelativePath(transformedAppFolder, zipFileDescriptor.getParentFile()));
     }
 
 }
