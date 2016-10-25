c14da75c4833219351ab6d7e56fb4fd365f92886
Mike Crockett <mcrockett@paypal.com>
2016-10-25 14:13:44 -0500

    Adding ability to pass maven invocation -D options.

--- butterfly-utilities/src/test/java/com/paypal/butterfly/utilities/maven/MavenGoalTest.java
+++ butterfly-utilities/src/test/java/com/paypal/butterfly/utilities/maven/MavenGoalTest.java
@@ -1,19 +1,25 @@
 package com.paypal.butterfly.utilities.maven;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyList;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import java.io.File;
-import java.util.Arrays;
+import java.util.Properties;
 
 import org.apache.maven.shared.invoker.DefaultInvocationRequest;
 import org.apache.maven.shared.invoker.InvocationRequest;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import com.paypal.butterfly.extensions.api.exception.TransformationDefinitionException;
 
 /**
  * @author mcrockett
@@ -30,22 +36,52 @@ public class MavenGoalTest {
     
     @BeforeClass
     public void classSetup() throws Exception {
+        String[] goals = {"a", "b", "c"};
         MockitoAnnotations.initMocks(this);
+        mavenGoal.setGoals(goals);
     }
 
     @Test
-    public void mavenGoalSetsRequstObject() {
+    public void setsRequestObject() {
+        Mockito.reset(request);
         String[] goals = {"a", "b", "c"};
         mavenGoal.setGoals(goals);
         mavenGoal.setFailAtEnd();
         mavenGoal.execution(null, null);
         verify(request, times(1)).setFailureBehavior(InvocationRequest.REACTOR_FAIL_AT_END);
         verify(request, times(1)).setPomFile(absoluteFile);
-        verify(request, times(1)).setGoals(Arrays.asList(goals));
+        verify(request, times(1)).setGoals(anyList());
     }
 
     @Test
-    public void mavenGoalProvidesADescription() {
+    public void doesntSetIfFailureBehaviorIsUnset() {
+        Mockito.reset(request);
+        mavenGoal.execution(null, null);
+        verify(request, times(0)).setFailureBehavior(anyString());
+    }
+
+    @Test(expectedExceptions = TransformationDefinitionException.class)
+    public void throwsIfTryAndSetPropertiesToNull() {
+        mavenGoal.setProperties(null);
+    }
+
+    @Test
+    public void doesntSetIfNullProperties() {
+        Mockito.reset(request);
+        mavenGoal.execution(null, null);
+        verify(request, times(0)).setProperties((Properties)any());
+    }
+
+    @Test
+    public void doesntSetIfEmptyProperties() {
+        Mockito.reset(request);
+        mavenGoal.setProperties(new Properties());
+        mavenGoal.execution(null, null);
+        verify(request, times(0)).setProperties((Properties)any());
+    }
+
+    @Test
+    public void providesADescription() {
         String[] goals = {"x", "y", "z"};
         MavenGoal mGoal = new MavenGoal(goals);
         Assert.assertEquals(mGoal.getDescription(), "Execute Maven goal [x, y, z] against pom file ");
