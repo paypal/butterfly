b07a56581fceb781bc0c4262371f069b192570e9
facarvalho <facarvalho@paypal.com>
2016-12-08 14:26:41 -0200

    Adding Spring usage to functional test sample app Plus, moving JAX-RS annotations from interface to implementation class

--- tests/sampleApp/sampleAppApi/src/main/java/com/tests/myapp/api/SampleResource.java
+++ tests/sampleApp/sampleAppApi/src/main/java/com/tests/myapp/api/SampleResource.java
@@ -1,7 +1,17 @@
 package com.tests.myapp.api;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.awt.*;
+
+@Path("/sampleresource")
 public interface SampleResource {
 
+    @GET
+    @Produces({ MediaType.TEXT_PLAIN })
+    @Path("/hello")
     String sayHello();
 
 }
--- tests/sampleApp/sampleAppService/src/main/java/com/tests/myapp/impl/SampleResourceImpl.java
+++ tests/sampleApp/sampleAppService/src/main/java/com/tests/myapp/impl/SampleResourceImpl.java
@@ -1,24 +1,17 @@
 package com.tests.myapp.impl;
 
 import com.tests.myapp.api.SampleResource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
 @Component
 @Scope("request")
-@Path("/sampleresource")
 public class SampleResourceImpl implements SampleResource {
 
-    public Hello hello = new Hello();
+    @Autowired
+    public Hello hello;
 
-    @GET
-    @Produces({ MediaType.TEXT_PLAIN })
-    @Path("/hello")
     @Override
     public String sayHello() { 	
         return hello.sayHello();
