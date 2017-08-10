57958dc9911491ad24b71b8986ea4a793cc794d3
facarvalho <facarvalho@paypal.com>
2017-08-10 11:10:10 -0500

    Improving package javadocs

---
+++ butterfly-utilities/src/main/java/com/paypal/butterfly/utilities/conditions/pom/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Transformation utility conditions to analyze and evaluate Maven POM files.
+ *
+ * @since 1.0.0
+ */
+package com.paypal.butterfly.utilities.conditions.pom;
--- butterfly-utilities/src/main/java/com/paypal/butterfly/utilities/package-info.java
+++ butterfly-utilities/src/main/java/com/paypal/butterfly/utilities/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Provides multiple transformation utilities, organized by type, each one having its own subpackage.
+ * Multiple transformation utilities, organized by type, each one having its own subpackage.
  * Transformation utilities are executed against the "to be" transformed project folder, but they do NOT apply
  * any modification to the project folder. Instead, they just gather information about it, which is
  * saved in the transformation context, to then be used later by other utilities or by operations.
