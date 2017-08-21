0c5518cad54c85af79e49106dc10feb277613b21
facarvalho <facarvalho@paypal.com>
2017-08-21 16:26:08 -0500

    Adding more javadoc package info

---
+++ butterfly-utilities/src/main/java/com/paypal/butterfly/utilities/operations/properties/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Transformation operations to manipulate properties files.
+ *
+ * @since 1.0.0
+ */
+package com.paypal.butterfly.utilities.operations.properties;
---
+++ butterfly-utilities/src/main/java/com/paypal/butterfly/utilities/operations/text/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Transformation operations to manipulate text files.
+ *
+ * @since 1.0.0
+ */
+package com.paypal.butterfly.utilities.operations.text;
