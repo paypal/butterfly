ec715893d08d057da88eb50b91226699933d381f
facarvalho <facarvalho@paypal.com>
2017-10-13 17:25:22 -0500

    Adding sample apps for tests

--- tests/sampleApp/sampleAppService/src/main/java/com/tests/myapp/impl/Hello.java
+++
@@ -1,9 +0,0 @@
-package com.tests.myapp.impl;
-
-public class Hello {
-
-    public String sayHello() {
-        return "Hello, World '" + Math.round(Math.random() * 1000) + "'!";
-    }
-
-}
--- tests/sampleApp/sampleAppService/src/main/java/com/tests/myapp/init/ApplicationConfig.java
+++
@@ -1,8 +0,0 @@
-package com.tests.myapp.init;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/v1/sampleapp/")
-public class ApplicationConfig extends Application {
-}
