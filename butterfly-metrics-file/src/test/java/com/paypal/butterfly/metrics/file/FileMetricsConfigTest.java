0d6623692e1cb1eff79dc3d87e12fff4cbfa3a6f
facarvalho <facarvalho@paypal.com>
2018-06-30 13:25:51 -0500

    #170 Replacing Cobertura by JaCoCo

---
+++ butterfly-metrics-file/src/test/java/com/paypal/butterfly/metrics/file/FileMetricsConfigTest.java
@@ -0,0 +1,12 @@
+package com.paypal.butterfly.metrics.file;
+
+import org.testng.annotations.Test;
+
+public class FileMetricsConfigTest {
+
+    @Test
+    public void test() {
+        // TODO
+    }
+
+}
---
+++ butterfly-test/src/test/java/com/paypal/butterfly/test/TransformationTestTest.java
@@ -0,0 +1,12 @@
+package com.paypal.butterfly.test;
+
+import org.testng.annotations.Test;
+
+public class TransformationTestTest {
+
+    @Test
+    public void test() {
+        // TODO
+    }
+
+}
