8686e01426231c7cb2d06dbda0634023dda843c7
myohn-paypal <72273452+myohn-paypal@users.noreply.github.com>
2020-11-17 13:14:08 -0800

    Adding support for loading extensions packaged as spring-boot uber jars. (#366)
    To make it easier for an extension to include its own transitive dependencies,
    change butterfly so that it can load extension jars that contain nested jars
    created by spring-boot maven plugin.
    
    Fixes #364

--- butterfly-cli-package/src/main/resources/butterfly/butterfly.cmd
+++ butterfly-cli-package/src/main/resources/butterfly/butterfly.cmd
@@ -6,4 +6,7 @@ if defined BUTTERFLY_HOME (
   set __BUTTERFLY_HOME__=%BUTTERFLY_HOME%
 )
 
-java -cp "%__BUTTERFLY_HOME__%\lib\*";"%__BUTTERFLY_HOME__%\extensions\*" com.paypal.butterfly.cli.ButterflyCliApp %*
+
+java -cp "%__BUTTERFLY_HOME__%\lib\*";"%__BUTTERFLY_HOME__%\extensions\*" ^
+    -Dloader.main=com.paypal.butterfly.cli.ButterflyCliApp ^
+    com.paypal.butterfly.cli.ButterflyCliApp %*
