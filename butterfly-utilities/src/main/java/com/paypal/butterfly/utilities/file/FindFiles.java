paypal/butterfly

commit d433ecd6b580a2d339126f10a8e39237c2920e2c

Upgrade various libraries and dependencies to latest versions (#375)
Upgrade various libraries, in particular:
spring-boot 1.4 -> 2.5
spring-framework 4.3 -> 5.3

Fixes issue #374

butterfly-utilities/src/main/java/com/paypal/butterfly/utilities/file/FindFiles.java
@@ -1,8 +1,11 @@
 package com.paypal.butterfly.utilities.file;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedHashSet;
 
 import com.paypal.butterfly.extensions.api.exception.TransformationUtilityException;
 import org.apache.commons.io.FileUtils;
@@ -307,7 +310,12 @@ public class FindFiles extends TransformationUtility<FindFiles> {
 
     @Override
     protected TUExecutionResult execution(File transformedAppFolder, TransformationContext transformationContext) {
-        final File searchRootFolder = getAbsoluteFile(transformedAppFolder, transformationContext);
+        final File searchRootFolder;
+        try {
+            searchRootFolder = getAbsoluteFile(transformedAppFolder, transformationContext).getCanonicalFile();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
 
         String _pathRegex = pathRegex;
         if (pathRegex != null && File.separatorChar != '/') {
@@ -351,6 +359,7 @@ public class FindFiles extends TransformationUtility<FindFiles> {
             }
             files.addAll(folders);
         }
+        files = new ArrayList<>(new LinkedHashSet<>(files));
 
         TUExecutionResult result;
 

.java-version
@@ -1,0 +1,1 @@
+1.8

build.gradle
@@ -1,6 +1,5 @@
-
 plugins {
-    id 'com.gradle.build-scan' version '2.1' 
+    id 'com.gradle.build-scan' version '3.6.2'
 }
 
 description = "Butterfly - Application transformation tool"
@@ -18,7 +17,6 @@ allprojects {
         mavenCentral()
         maven{
             url "https://repo.spring.io/plugins-release/"
-            url "https://dl.bintray.com/g00fy2/maven"
         }
     }
 
@@ -227,26 +225,26 @@ ext.lib = [
 
         // Java specs
         annotation_api: "javax.annotation:javax.annotation-api:1.3.2",
-        jaxrs: "javax.ws.rs:javax.ws.rs-api:2.1",
+        jaxrs: "javax.ws.rs:javax.ws.rs-api:2.1.1",
         bean_validation: "javax.validation:validation-api:2.0.1.Final",
 
         // Spring
-        spring_context: "org.springframework:spring-context:4.3.2.RELEASE",
-        spring_boot_starter: "org.springframework.boot:spring-boot-starter:1.4.0.RELEASE",
-        spring_boot_autoconfigure: "org.springframework.boot:spring-boot-autoconfigure:1.4.0.RELEASE",
-        spring_boot_loader: "org.springframework.boot:spring-boot-loader:1.4.0.RELEASE",
-        spring_test: "org.springframework:spring-test:4.3.2.RELEASE",
+        spring_context: "org.springframework:spring-context:5.3.7",
+        spring_boot_starter: "org.springframework.boot:spring-boot-starter:2.5.0",
+        spring_boot_autoconfigure: "org.springframework.boot:spring-boot-autoconfigure:2.5.0",
+        spring_boot_loader: "org.springframework.boot:spring-boot-loader:2.5.0",
+        spring_test: "org.springframework:spring-test:5.3.7",
 
         // Maven
-        maven_model: "org.apache.maven:maven-model:3.5.4",
-        maven_invoker: "org.apache.maven.shared:maven-invoker:3.0.1",
+        maven_model: "org.apache.maven:maven-model:3.8.1",
+        maven_invoker: "org.apache.maven.shared:maven-invoker:3.1.0",
 
         // Parsers
-        gson: "com.google.code.gson:gson:2.7",
-        woodstox_core: "com.fasterxml.woodstox:woodstox-core:5.0.3",
+        gson: "com.google.code.gson:gson:2.8.7",
+        woodstox_core: "com.fasterxml.woodstox:woodstox-core:6.2.6",
         xmlunit: "xmlunit:xmlunit:1.5",
-        yamlbeans: "com.esotericsoftware.yamlbeans:yamlbeans:1.08",
-        javaparser_core: "com.github.javaparser:javaparser-core:3.15.14",
+        yamlbeans: "com.esotericsoftware.yamlbeans:yamlbeans:1.15",
+        javaparser_core: "com.github.javaparser:javaparser-core:3.22.1",
         jackson: "com.fasterxml.jackson.core:jackson-annotations:2.9.8",
         jackson_databind: "com.fasterxml.jackson.core:jackson-databind:2.9.8",
 
@@ -255,27 +253,30 @@ ext.lib = [
 
         // Utilities
         annotations: "com.google.code.findbugs:annotations:3.0.0",
-        classgraph: "io.github.classgraph:classgraph:4.8.90",
-        commons_io: "commons-io:commons-io:2.5",
-        commons_lang3: "org.apache.commons:commons-lang3:3.7",
-        commons_collections4: "org.apache.commons:commons-collections4:4.0",
-        guava: "com.google.guava:guava:30.0-jre",
-        jopt_simple: "net.sf.jopt-simple:jopt-simple:5.0.2",
-        plexus_utils: "org.codehaus.plexus:plexus-utils:3.1.0",
-        reflections: "org.reflections:reflections:0.9.10",
-        slf4j_api: "org.slf4j:slf4j-api:1.7.21",
-        log4j_api: "org.apache.logging.log4j:log4j-api:2.14.0",
-        log4j_core:"org.apache.logging.log4j:log4j-core:2.14.0",
-        log4j_slf4j_impl: "org.apache.logging.log4j:log4j-slf4j-impl:2.14.0",
+        classgraph: "io.github.classgraph:classgraph:4.8.107",
+        commons_io: "commons-io:commons-io:2.9.0",
+        commons_lang3: "org.apache.commons:commons-lang3:3.12.0",
+        commons_collections4: "org.apache.commons:commons-collections4:4.4",
+        guava: "com.google.guava:guava:30.1.1-jre",
+        jopt_simple: "net.sf.jopt-simple:jopt-simple:5.0.4",
+        plexus_utils: "org.codehaus.plexus:plexus-utils:3.2.1",
+        reflections: "org.reflections:reflections:0.9.12",
+        slf4j_api: "org.slf4j:slf4j-api:1.7.30",
+        log4j_api: "org.apache.logging.log4j:log4j-api:2.14.1",
+        log4j_core:"org.apache.logging.log4j:log4j-core:2.14.1",
+        log4j_slf4j_impl: "org.apache.logging.log4j:log4j-slf4j-impl:2.14.1",
         //for async log4j2 logging
-        disruptor:"com.lmax:disruptor:3.3.4",
-        zip4j: "net.lingala.zip4j:zip4j:1.3.2",
-        version_compare :"com.g00fy2:versioncompare:1.3.7",
+        disruptor: "com.lmax:disruptor:3.4.4",
+        zip4j: "net.lingala.zip4j:zip4j:2.8.0",
+        version_compare :"io.github.g00fy2:versioncompare:1.4.1",
 
         // Swagger
         swagger_annotations: "io.swagger.core.v3:swagger-annotations:2.0.8",
 
         // Tests
+        hamcrest: "org.hamcrest:hamcrest-core:1.3",
+        javassist: "org.javassist:javassist:3.26.0-GA",
+        objenesis: "org.objenesis:objenesis:2.2",
         testng: "org.testng:testng:6.14.2",
         mockito_all: "org.mockito:mockito-all:1.10.19",
         powermock_module_testng: "org.powermock:powermock-module-testng:1.6.5",

butterfly-cli/build.gradle
@@ -12,11 +12,17 @@ dependencies {
             lib.commons_io,
             lib.log4j_api,
             lib.log4j_core,
-            lib.log4j_slf4j_impl,
             lib.disruptor
+    compile (lib.log4j_slf4j_impl) {
+        // favor newer 1.7.30
+        exclude(module: 'slf4j-api')
+    }
     compile (lib.spring_boot_starter) {
         exclude(module: 'commons-logging')
-        exclude(module:'logback-classic')
+        exclude(module: 'logback-classic')
+
+        // don't redirect log4j2 logs to slf4j because we bind slf4j to log4j2...
+        exclude(module: 'log4j-to-slf4j')
     }
     testCompile project(':butterfly-utilities')
     testCompile(lib.testng) {
@@ -24,8 +30,27 @@ dependencies {
         exclude(module: 'guava')
     }
     testCompile lib.mockito_all,
-            lib.powermock_module_testng,
-            lib.powermock_api_mockito
+                lib.hamcrest,
+                lib.javassist,
+                lib.objenesis
+    testCompile(lib.powermock_api_mockito) {
+        // favor version pulled in by lib.hamcrest
+        exclude(module: 'hamcrest-core')
+
+        // favor version pulled in by lib.javassist
+        exclude(module: 'javassist')
+
+        // favor version pulled in by lib.objenesis
+        exclude(module: 'objenesis')
+    }
+
+    testCompile(lib.powermock_module_testng) {
+        // favor version pulled in by lib.testng
+        exclude(module: 'testng')
+        
+        // favor version pulled in by lib.javassist
+        exclude(module: 'javassist')
+    }
 }
 
 jar {

butterfly-cli/src/main/java/com/paypal/butterfly/cli/logging/Log4j2LogConfigurator.java
@@ -61,14 +61,12 @@ public class Log4j2LogConfigurator extends LogConfigurator {
     public void setVerboseMode(boolean verboseMode) {
         this.verboseMode = verboseMode;
         if (verboseMode) {
-            Layout layout = PatternLayout.newBuilder().withConfiguration(config).withPattern("[%d{HH:mm:ss.SSS}] [%highlight(%level)] %msg%n").build();
+            Layout layout = PatternLayout.newBuilder().withConfiguration(config).withPattern("[%d{HH:mm:ss.SSS}] [%highlight{%level}] %msg%n").build();
             Appender consoleAppender = ConsoleAppender.createDefaultAppenderForLayout(layout);
             consoleAppender.start();
             config.getLoggerConfig("com.paypal.butterfly.cli").removeAppender("CONSOLE");
             config.getRootLogger().addAppender(consoleAppender,null,null);
             context.updateLoggers();
-      //  } else {
-          // TODO
         }
     }
 

butterfly-core/build.gradle
@@ -8,6 +8,7 @@ dependencies {
     compile lib.annotation_api,
             lib.slf4j_api,
             lib.gson,
+            lib.guava,
             lib.spring_boot_loader,
             lib.classgraph,
             lib.reflections,

butterfly-core/src/main/java/com/paypal/butterfly/core/CompressionHandler.java
@@ -1,17 +1,16 @@
 package com.paypal.butterfly.core;
 
-import java.io.File;
-
+import com.paypal.butterfly.api.TransformationResult;
+import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.model.ZipParameters;
+import net.lingala.zip4j.model.enums.CompressionLevel;
+import net.lingala.zip4j.model.enums.CompressionMethod;
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import com.paypal.butterfly.api.TransformationResult;
-
-import net.lingala.zip4j.core.ZipFile;
-import net.lingala.zip4j.model.ZipParameters;
-import net.lingala.zip4j.util.Zip4jConstants;
+import java.io.File;
 
 /**
  * This bean takes care of compressing the output folder,
@@ -34,8 +33,8 @@ class CompressionHandler {
             ZipFile zipFile = new ZipFile(compressedFile);
             ZipParameters parameters = new ZipParameters();
 
-            parameters.setCompressionMethod(Zip4jConstants.COMP_DEFLATE);
-            parameters.setCompressionLevel(Zip4jConstants.DEFLATE_LEVEL_NORMAL);
+            parameters.setCompressionMethod(CompressionMethod.DEFLATE);
+            parameters.setCompressionLevel(CompressionLevel.NORMAL);
 
             zipFile.addFolder(inputFile, parameters);
             FileUtils.deleteDirectory(transformationResult.getTransformedApplicationDir());

commit a9d8baeaaaac2b4b545e168014b01ab609fdd241

Fixing FindFiles files bug introduced in #375 (#391)
Partially reverting https://github.com/paypal/butterfly/commit/887e3e2fbb7ebc8de65f991f92220b696f83ae7a.
Change FindFiles to not return cannonical file path, undoing behavior change
Introduced in 3.1.0 version and restoring back to 3.0.0 version.
Newer commons-io library return canonical results by default.

butterfly-utilities/src/main/java/com/paypal/butterfly/utilities/file/FindFiles.java
@@ -310,12 +310,7 @@ public class FindFiles extends TransformationUtility<FindFiles> {
 
     @Override
     protected TUExecutionResult execution(File transformedAppFolder, TransformationContext transformationContext) {
-        final File searchRootFolder;
-        try {
-            searchRootFolder = getAbsoluteFile(transformedAppFolder, transformationContext).getCanonicalFile();
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
-        }
+        final File searchRootFolder = getAbsoluteFile(transformedAppFolder, transformationContext);
 
         String _pathRegex = pathRegex;
         if (pathRegex != null && File.separatorChar != '/') {
@@ -350,7 +345,7 @@ public class FindFiles extends TransformationUtility<FindFiles> {
             Collection<File> allFolders = FileUtils.listFilesAndDirs(searchRootFolder, new NotFileFilter(TrueFileFilter.INSTANCE), (recursive ? TrueFileFilter.INSTANCE : DirectoryFileFilter.DIRECTORY));
             allFolders.remove(searchRootFolder);
             for (File folder : allFolders) {
-                if (!recursive && !folder.getParentFile().equals(searchRootFolder)) {
+                if (!recursive && !fileEquals(folder.getParentFile(), searchRootFolder)) {
                     continue;
                 }
                 if (filter.accept(folder)) {
@@ -377,4 +372,16 @@ public class FindFiles extends TransformationUtility<FindFiles> {
         return result;
     }
 
+    private boolean fileEquals(File left, File right) {
+        if (left.equals(right)) {
+            return true;
+        }
+        try {
+            File canonicalLeft = left.getCanonicalFile();
+            File canonicalRight = right.getCanonicalFile();
+            return canonicalLeft.equals(canonicalRight);
+        } catch (IOException e) {
+            return false;
+        }
+    }
 }